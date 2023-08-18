3381671d47f36eeaa3871be1b8be394d8b2dd8be
ryan-williams <nobigdealstyle@gmail.com>
2023-08-18 20:28:52 +0000

    Deploying to gh-pages from @ ryan-williams/apvd@e766c4abd86461bfe5f7a783a87bc0bde75f1747 🚀

---
+++ _next/static/Tru05krSrQ0aRmAT_vgC7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d60dd81bee3b615f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
