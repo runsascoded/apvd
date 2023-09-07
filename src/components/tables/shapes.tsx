e0e629a800db101689b948047253632756968fad
Ryan Williams <ryan@runsascoded.com>
2023-09-07 09:09:39 -0400

    shade skipped ShapesTable vars

--- src/components/tables/shapes.tsx
+++ src/components/tables/shapes.tsx
@@ -1,13 +1,31 @@
-import {getCenter, getRadii, S, shapeType} from "../../lib/shape";
-import React from "react";
+import {getCenter, getRadii, map, S, shapeType} from "../../lib/shape";
+import React, {ReactNode} from "react";
+import {Vars} from "../../lib/vars";
+import css from "./shapes.module.scss"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 export type Props = {
     shapes: S[]
+    vars: Vars
+    precision: number
 }
 
-export function ShapesTable({ shapes }: Props) {
+export function VarCell({ skipped, children }: { skipped: boolean, children: ReactNode }) {
     return (
-        <table>
+        skipped
+            ? <OverlayTrigger overlay={<Tooltip>Fixed/disabled</Tooltip>}>
+                <td className={css.skipped}>
+                    <span>{children}</span>
+                </td>
+            </OverlayTrigger>
+            : <td>{children}</td>
+    )
+}
+
+export function ShapesTable({ shapes, vars, precision = 4 }: Props) {
+    return (
+        <table className={css.shapesTable}>
             <thead>
             <tr>
                 <th>Name</th>
@@ -19,15 +37,26 @@ export function ShapesTable({ shapes }: Props) {
             </tr>
             </thead>
             <tbody>{
-                shapes.map(({ idx, name, ...shape }) => {
+                shapes.map(({ idx, name, ...shape }, shapeIdx) => {
+                    const skippedVars = vars.skipVars[shapeIdx] || []
                     const c = getCenter(shape)
                     const [ rx, ry ] = getRadii(shape)
+                    const skipCx = skippedVars.includes("x")
+                    const skipCy = skippedVars.includes("y")
+                    const [ skipRx, skipRy ] = map(
+                        shape,
+                        c => {
+                            const skip = skippedVars.includes("r")
+                            return [skip, skip]
+                        },
+                        e => [ skippedVars.includes("rx"), skippedVars.includes("ry") ]
+                    )
                     return <tr key={idx}>
                         <td style={{ textAlign: "right", }}>{name}</td>
-                        <td>{c.x.toPrecision(4)}</td>
-                        <td>{c.y.toPrecision(4)}</td>
-                        <td>{rx.toPrecision(4)}</td>
-                        <td>{ry.toPrecision(4)}</td>
+                        <VarCell skipped={skipCx}>{c.x.toPrecision(precision)}</VarCell>
+                        <VarCell skipped={skipCy}>{c.y.toPrecision(precision)}</VarCell>
+                        <VarCell skipped={skipRx}>{ rx.toPrecision(precision)}</VarCell>
+                        <VarCell skipped={skipRy}>{ ry.toPrecision(precision)}</VarCell>
                         <td>{shapeType(shape)}</td>
                     </tr>
                 })
