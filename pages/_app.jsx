65661629225d8ddf98c3442e9a3d0c6d27b77349
Ryan Williams <ryan@runsascoded.com>
2023-09-17 13:10:10 -0400

    add favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import Head from "next/head";
+import {getBasePath} from "next-utils/basePath";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const basePath = getBasePath()
+  return (
+      <>
+        <Head>
+          <link rel="shortcut icon" href={`${basePath}/img/screenshots/3-circles.png`} />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
 
 export default MyApp
