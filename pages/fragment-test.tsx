8bd05b757fed2309e8d5fdabf5a1f0fbeabbb74a
Ryan Williams <ryan@runsascoded.com>
2023-09-25 23:03:47 -0400

    fragment state prototype

---
+++ pages/fragment-test.tsx
@@ -0,0 +1,32 @@
+import {intParam, Param, ParsedParam, parseHashParams} from "../src/lib/params";
+
+type Params = {
+    n: Param<number>
+    o: Param<number>
+}
+
+type ParsedParams = {
+    n: ParsedParam<number>
+    o: ParsedParam<number>
+}
+
+export default function Page() {
+    const params: Params = {
+        n: intParam(0),
+        o: intParam(0),
+    }
+
+    const {
+        n: [ n, setN ],
+        o: [ o, setO ],
+    }: ParsedParams = parseHashParams({ params })
+
+    return (
+        <div>
+            <h1>Fragment Test</h1>
+            <p>Fragment Test: {n} {o}</p>
+            <input type={"number"} value={n} onChange={e => setN(parseInt(e.target.value))} />
+            <input type={"number"} value={o} onChange={e => setO(parseInt(e.target.value))} />
+        </div>
+    )
+}
